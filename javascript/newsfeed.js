85c3aaff1a7dda2a9471a814f2c377838ae575fb
nirokay <80123634+nirokay@users.noreply.github.com>
2023-12-04 10:45:05 +0100

    implemented wildcard year for newsfeed

--- javascript/newsfeed.js
+++ javascript/newsfeed.js
@@ -15,7 +15,7 @@ function getHtml(element) {
     html += "<h4>" + element.name + "</h4>";
 
     // Date range:
-    html += "<h5>von " + element.from + " bis " + element.till + "</h5>";
+    html += "<h5>von " + readable(element.from) + " bis " + readable(element.till) + "</h5>";
 
     // Details:
     html += "<p class='generic-center'>" + element.details.join('<br />') + "</p>";
@@ -38,34 +38,55 @@ function fetchNews() {
         .then(console.log("Got stuff!"));
 }
 
-function isRelevant(element) {
-    let now = new Date();
-    function toUgly(time) {
-        let temp = time.split("-");
-        // Wildcard for every-year events:
-        if(temp[2] == "*") {
-            temp[2] = now.getFullYear();
-        }
-        return [temp[1], temp[2], temp[0]].join("/");
+function normalize(date) {
+    /** Converts good format "yyyy-MM-dd" to ugly format "MM/dd/yyyy" */
+    const now = new Date();
+    let d = date.split("-");
+    // Wildcard for every-year events:
+    if(d[0] == "*") {
+        d[0] = now.getFullYear();
     }
-    let unixFrom = new Date(toUgly(element.from)).getTime();
-    let unixTill = new Date(toUgly(element.till)).getTime();
+    console.log(d)
+    return [d[1], d[2], d[0]].join("/");
+}
+
+function readable(date) {
+    /** Gets the date in a more readable format "dd.MM.yyyy" from "yyyy-MM-dd" */
+    let now = new Date();
+    let d = date.split("-");
+    let
+        year = d[0],
+        month = d[1],
+        day = d[2]
+    if(year == "*") { year = now.getFullYear() }
+    return [day, month, year].join(".");
+}
+
+function isRelevant(element) {
+    /** Filters irrelevant news (depending on the date) */
+    let now = new Date();
+    let unixFrom = new Date(normalize(element.from)).getTime();
+    let unixTill = new Date(normalize(element.till)).getTime() + 86400000;
     let unixNow = now.getTime();
+    console.log([unixNow, "----", unixFrom, unixTill].join("\n"))
     return unixNow <= unixTill && unixNow >= unixFrom;
 }
 
 function refreshNews() {
+    /** Refreshes `news` variable from remote repo */
     console.log("Refreshing news from remote repo...");
     fetchNews();
     getDiv().innerHTML = "";
-    console.log(news);
-    news.forEach(element => {
-        if(isRelevant(element)) {
-            addElement(element);
-        }
+
+    let relevantNews = news.filter(element => isRelevant(element));
+    relevantNews.forEach(element => {
+        addElement(element);
     });
-    if(news.length == 0) {
-        addToDiv("<p>Derzeit sind keine Neuigkeiten vorhanden.<br />Klicke auf \"Neu laden\", um zu sehen ob doch Neuigkeiten vorhanden sind!</p>");
+    if(relevantNews.length == 0) {
+        addToDiv(
+            "<p>Derzeit sind keine Neuigkeiten vorhanden.<br />" +
+            "Klicke auf \"Neu laden\", um zu sehen ob doch Neuigkeiten vorhanden sind!</p>"
+        );
     }
 }
 
