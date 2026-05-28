bc0c5d7c85886e40ace8c32d335095b585d99c05
nirokay <nirokay-public@protonmail.com>
2026-05-28 09:14:47 +0200

    map compiling

--- website/map.nim
+++ website/map.nim
@@ -21,15 +21,15 @@ var
     locations: seq[Location] = getLocationsSorted()
     picture: HtmlElement = img(svgExportPath, "Karte wird geladen...").add(
         "usemap" <=> "#location-map",
-        "width" <=> px(mapScaleTo),
-        "height" <=> px(mapScaleTo)
+        "width" <=> $mapScaleTo & "px",
+        "height" <=> $mapScaleTo & "px"
     ).setStyle(
         "border-radius" := "20px",
         "text-align" := "center",
         "color" := colourText,
-        "width" := px(mapScaleTo),
-        "height" := px(mapScaleTo),
-        "margin" := px(0)
+        "width" := $mapScaleTo & "px",
+        "height" := $mapScaleTo & "px",
+        "margin" := "0px"
     )
 
 var areas: seq[string]
@@ -41,8 +41,8 @@ for location in locations.withCoords():
 
     # Shape:
     case coords.len():
-    of 3: area.add attr("shape", "circle")
-    of 4: area.add attr("shape", "rect")
+    of 3: area.add "shape" <=> "circle"
+    of 4: area.add "shape" <=> "rect"
     else:
         raise ValueError.newException(
             "Got a length of " & $coords.len() & " for coordinates! Expected 3-4. " &
@@ -64,13 +64,14 @@ for location in locations.withCoords():
     )
 
     # Dirty quick-fix for weird behaviour:
-    area.tagAttributes = area.tagAttributes.deduplicate()
+    area.attributes = area.attributes.deduplicate()
 
     # Add to sequence:
     areas.add($area)
 
-var map: HtmlElement = newHtmlElement("map", areas.join("\n"))
-    .add(attr("name", "location-map"))
+var map: HtmlElement = newHtmlElement("map", html areas.join("\n")).add(
+    "name" <=> "location-map"
+)
 
 var locationButtons: seq[HtmlElement]
 for location in locations:
@@ -78,7 +79,7 @@ for location in locations:
 
 html.addToBody(
     divSpacerTop, # TODO: Fix this dirty hack, someday
-    h1("Karte von Herzogsägmühle"),
+    h1(html "Karte von Herzogsägmühle"),
     pc("Diese Karte ist interaktiv. Du kannst jede Stecknadel/Grau-Schwarzes Rechteck anklicken und zu dem entsprechenden Ort gelangen."),
     insertButtons(hrefIndex),
     `div`(
@@ -87,10 +88,10 @@ html.addToBody(
     ).setClass(centerClass).setStyle(
         "overflow" := "scroll",
         "touch-action" := "pan-x pan-y pinch-zoom",
-        "max-width" := px(pictureDimensions.maxWidth),
-        "max-height" := px(pictureDimensions.maxHeight),
-        "width" := $(pictureDimensions.width) & "%",
-        "height" := $(pictureDimensions.height) & "vh",
+        "max-width" := $pictureDimensions.maxWidth & "px",
+        "max-height" := $pictureDimensions.maxHeight & "px",
+        "width" := $pictureDimensions.width & "%",
+        "height" := $pictureDimensions.height & "vh",
         "border-radius" := "20px",
         "margin" := "10px auto"
     ),
@@ -105,5 +106,5 @@ stdout.write "\r📌 Finished generating big map\n"
 stdout.flushFile()
 
 html.add ogImage(urlImages & "map.svg")
-html.setStylesheet(css)
+html.applyStylesheet(css)
 html.generate()
