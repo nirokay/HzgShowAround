3213885b765e03cb4b3ca764735f66c486064330
nirokay <nirokay-public@protonmail.com>
2026-09-03 09:35:14 +0200

    fucky wucky

--- website/locations.nim
+++ website/locations.nim
@@ -321,4 +321,3 @@ proc generateLocations*(locations: seq[Location]) =
 
     # Write lookup-table to disk:
     locationLookupTableFile.writeFile($%locationLookupTable)
-    # locationLookupTableFile.writeFile(pretty(%locationLookupTable, 4))
